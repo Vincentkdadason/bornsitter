bbcb38605d945e230fd4df8257b8589adb6b05f2
vincent <vincentmichuki@gmail.com>
2019-02-10 12:50:11 +0300

    resp

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
+    const bs_nav = $('.bs-nav');
+    // show side menu
+    function showSideBar() {
+        console.log(window.innerWidth);
+        return window.innerWidth < 1050;
+    }
+    showSideBar() ? bs_nav.addClass('side-menu') : bs_nav.removeClass('side-menu');
+
+    $(window).on('resize', function () {
+        showSideBar() ? bs_nav.addClass('side-menu') : bs_nav.removeClass('side-menu');
+    });
+
+    $('.toggle-menu').on('click', function () {
+       $('.top-menu').toggleClass('slide-right');
+    });
+
     $(".owl-carousel").owlCarousel({
         loop:true,
         margin:0,
